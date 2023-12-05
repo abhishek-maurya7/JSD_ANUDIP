8bae1285fac686070ff036e612e04f016b4ed7bc
Abhishek Maurya <mauryaabhishek279@gmail.com>
2023-12-05 19:19:58 +0530

    added name to prints

--- Java/BankAccount.java
+++ Java/BankAccount.java
@@ -32,6 +32,7 @@ class BankAccount {
         //Creating instance of bankAccount class and passing values to the constructor
         BankAccount ICICI = new BankAccount("Abhishek Maurya", "ICICI", 15000);
         //calling methods and printing returned values
+        System.out.println("Account Holder: " + ICICI.accountHolderName);
         System.out.println("ICICI Current Balance: " + ICICI.getBalance());
         System.out.println("ICICI Balance after deposit: " + ICICI.deposit(1200));
         System.out.println("ICICI Balance after withdrwal: " + ICICI.withdraw(100));
@@ -40,6 +41,7 @@ class BankAccount {
         //Creating instance of bankAccount class and passing values to the constructor
         BankAccount HDFC = new BankAccount("John Doe", "HDFC", 20000);
         //calling methods and printing returned values
+        System.out.println("Account Holder: " + HDFC.accountHolderName);
         System.out.println("HDFC Current Balance: " + HDFC.getBalance());
         System.out.println("HDFC Balance after deposit: " + HDFC.deposit(1200));
         System.out.println("HDFC Balance after withdrwal: " + HDFC.withdraw(100));
@@ -48,6 +50,7 @@ class BankAccount {
         //Creating instance of bankAccount class and passing values to the constructor
         BankAccount SBI = new BankAccount("ABC XYZ", "SBI", 5000);
         //calling methods and printing returned values
+        System.out.println("Account Holder: " + SBI.accountHolderName);
         System.out.println("SBI Current Balance: " + SBI.getBalance());
         System.out.println("SBI Balance after deposit: " + SBI.deposit(1200));
         System.out.println("SBI Balance after withdrwal: " + SBI.withdraw(100));
